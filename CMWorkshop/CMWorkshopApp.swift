8186dbaaa94b513f916335bb9fc98c5604af8097
Nat Nopphawong <nopphawong.n@gmail.com>
2020-07-20 15:12:28 +0700

    Initial Commit

---
+++ CMWorkshop/CMWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMWorkshopApp.swift
+//  CMWorkshop
+//
+//  Created by Codemobiles Golf on 20/7/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct CMWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CMWorkshop/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CMWorkshop
+//
+//  Created by Codemobiles Golf on 20/7/2563 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
