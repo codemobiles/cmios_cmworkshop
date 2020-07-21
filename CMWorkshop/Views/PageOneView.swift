05aae509a50c05e78ff60e433f0820ba45b5d873
Nat Nopphawong <nopphawong.n@gmail.com>
2020-07-21 14:39:42 +0700

    Ep7# Feed Data with Alamofire

--- CMWorkshop/Views/PageOneView.swift
+++ CMWorkshop/Views/PageOneView.swift
@@ -6,10 +6,28 @@
 //
 
 import SwiftUI
+import Alamofire
 
 struct PageOneView: View {
     var body: some View {
         Text("Page One")
+            .onAppear {
+                self.feedData()
+            }
+    }
+    
+    func feedData() {
+        let params = ["username": "admin", "password": "password", "type": "foods"]
+        let url = "http://codemobiles.com/adhoc/youtubes/index_new.php"
+        AF.request(url, method: .post, parameters: params).responseJSON { (response) in
+            switch response.result {
+            case .success:
+                print(response.value)
+            case .failure(let err):
+                print(err.responseCode)
+            
+            }
+        }
     }
 }
 
