a80bff512d2d43158bf75f057171eb21ea8d561c
Nat Nopphawong <nopphawong.n@gmail.com>
2020-07-21 17:11:32 +0700

    Ep8# Youtube Data and Mock Row List

--- CMWorkshop/Views/PageOneView.swift
+++ CMWorkshop/Views/PageOneView.swift
@@ -9,11 +9,61 @@ import SwiftUI
 import Alamofire
 
 struct PageOneView: View {
+    @State private var youtubeDataArray: [Youtube] = []
     var body: some View {
-        Text("Page One")
-            .onAppear {
-                self.feedData()
+        List {
+            VStack(alignment: .leading) {
+                HStack {
+                    Image("ic_face_id")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Title")
+                        Text("Subtitle")
+                    }
+                }
+                Image("ios_training_header")
+                    .resizable()
+                    .aspectRatio(1.7, contentMode: .fill)
+                    .frame(minWidth: 0, maxWidth: .infinity)
             }
+            VStack(alignment: .leading) {
+                HStack {
+                    Image("ic_face_id")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Title")
+                        Text("Subtitle")
+                    }
+                }
+                Image("ios_training_header")
+                    .resizable()
+                    .aspectRatio(1.7, contentMode: .fill)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+            }
+            VStack(alignment: .leading) {
+                HStack {
+                    Image("ic_face_id")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Title")
+                        Text("Subtitle")
+                    }
+                }
+                Image("ios_training_header")
+                    .resizable()
+                    .aspectRatio(1.7, contentMode: .fill)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+            }
+        }
+        .onAppear {
+            self.feedData()
+        }
     }
     
     func feedData() {
@@ -23,9 +73,15 @@ struct PageOneView: View {
             switch response.result {
             case .success:
                 print(response.value)
+                do {
+                    let result = try JSONDecoder().decode(YoutubeResponse.self, from: response.data!)
+                    self.youtubeDataArray.removeAll()
+                    self.youtubeDataArray = result.youtubes
+                } catch {
+                    print(error.localizedDescription)
+                }
             case .failure(let err):
                 print(err.responseCode)
-            
             }
         }
     }
