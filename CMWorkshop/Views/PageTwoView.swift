d65a1ba2f6a29daf2b223bb6a130b1825c79d030
Nat Nopphawong <nopphawong.n@gmail.com>
2020-07-23 16:37:00 +0700

    Ep13# Modifier Button Image

--- CMWorkshop/Views/PageTwoView.swift
+++ CMWorkshop/Views/PageTwoView.swift
@@ -9,12 +9,47 @@ import SwiftUI
 
 struct PageTwoView: View {
     var body: some View {
-        Text("Page Two ")
+        GeometryReader { geometry in
+            VStack {
+                Image("upload_image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: geometry.size.width / 1.5, height: geometry.size.width / 1.5)
+                    .clipShape(Circle())
+                    .padding(.bottom, 35)
+                HStack(spacing: 60) {
+                    Button(action: {
+                        
+                    }) {
+                        ImageButtonView(image: "camera_normal", geometry: geometry)
+                    }
+                    
+                    Button(action: {
+                        
+                    }) {
+                        ImageButtonView(image: "share_normal", geometry: geometry)
+                    }
+                }
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)
+        }
     }
 }
 
 struct PageTwoView_Previews: PreviewProvider {
     static var previews: some View {
         PageTwoView()
+    }
+}
+
+struct ImageButtonView: View {
+    let image: String
+    let geometry: GeometryProxy
+    var body: some View {
+        Image("camera_normal")
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: geometry.size.width / 3.5)
     }
 }
